87339ef6e2e0215243e91812d9e44c40a996e12c
Oliver Schoenecker <oliver_schoenecker@gmx.de>
2026-02-15 17:11:51 +0100

    initial commit

---
+++ src/main/java/com/start/Main.java
@@ -0,0 +1,24 @@
+package com.start;
+
+import java.io.file;
+import javax.sound.sampled.audioFileFormat.Type;
+import javax.sound.sampled.AudioSystem;
+import java.applet.AudioClip;
+
+/* - Es muss erkannt werden, ob überhaupt Dateien in einem Ordner existieren
+        -> Fehler catch
+   - wenn welche vorhanden sind, ob diese auch gültige Sounddateien sind
+        -> Fehler catch
+   - wenn vorhanden abspielen  
+ */
+
+public class Main {
+
+    public static fileDetect(String fileName){    
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
